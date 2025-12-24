3a6134a3d7d7479e91df96435cfe6dd3c0a9c379
X69202453 <milos.davmi.davitkovic@bit.admin.ch>
2025-12-24 21:06:36 +0100

    Clean up

--- jeap-initializer/src/test/java/ch/admin/bit/jeap/initializer/TestUtils.java
+++ jeap-initializer/src/test/java/ch/admin/bit/jeap/initializer/TestUtils.java
@@ -2,17 +2,12 @@ package ch.admin.bit.jeap.initializer;
 
 import ch.admin.bit.jeap.initializer.model.ProjectRequest;
 import ch.admin.bit.jeap.initializer.model.ProjectTemplate;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-
-@Slf4j
 public class TestUtils {
 
     public static void writeToFile(Path srcDirectory, String fileName, String content) throws IOException {
@@ -33,14 +28,7 @@ public class TestUtils {
     }
 
     public static void addTestFileToFolder(String fileName, Path path) throws IOException {
-        final Path targetFile = path.resolve(fileName);
-
-        Files.createDirectories(targetFile.getParent());
-
-        try (InputStream inputStream = new ClassPathResource(fileName).getInputStream()) {
-            Files.copy(inputStream, targetFile, REPLACE_EXISTING);
-        }
-
-        log.info("Add a file [{}] to the folder [{}] on the path [{}]", fileName, path, targetFile);
+        Path targetFile = path.resolve(fileName);
+        Files.copy(new ClassPathResource("./" + fileName).getInputStream(), targetFile);
     }
 }
