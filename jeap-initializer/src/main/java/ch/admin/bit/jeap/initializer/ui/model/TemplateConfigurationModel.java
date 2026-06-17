be0cf877136a7ed3833e30c207f633128598769d
Laris Jacobs <laris.jacobs@bit.admin.ch>
2026-06-17 10:54:10 +0200

    JEAP-7164 Fixed sonar issues and refactored deprecated spring boot starter.

--- jeap-initializer/src/main/java/ch/admin/bit/jeap/initializer/ui/model/TemplateConfigurationModel.java
+++ jeap-initializer/src/main/java/ch/admin/bit/jeap/initializer/ui/model/TemplateConfigurationModel.java
@@ -14,22 +14,34 @@ import java.util.Set;
 @Valid
 public class TemplateConfigurationModel {
 
+    @SuppressWarnings("java:S5998")
+    private static final String JAVA_IDENTIFIER = "[a-zA-Z][a-zA-Z0-9]*";
+    @SuppressWarnings("java:S5998")
+    private static final String PACKAGE_OR_GROUP_ID = JAVA_IDENTIFIER + "(?:\\." + JAVA_IDENTIFIER + ")*";
+    @SuppressWarnings("java:S5998")
+    private static final String ARTIFACT_ID = JAVA_IDENTIFIER + "(?:-" + JAVA_IDENTIFIER + ")*";
+
     private Set<String> selectedModuleIds = new HashSet<>();
 
     @NotBlank(message = "Please enter a system name")
     private String systemName = "jme";
+
     @NotBlank(message = "Please enter a department name")
     private String department = "BIT";
+
     @NotBlank(message = "Please enter a human-readable application name")
     private String applicationName = "jEAP Project";
+
     @NotBlank(message = "Please enter the base package")
-    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*(\\.[a-zA-Z0-9]+)*$", message = "Please enter a valid package name")
+    @Pattern(regexp = PACKAGE_OR_GROUP_ID, message = "Please enter a valid package name")
     private String basePackage = "ch.admin.bit.jme";
+
     @NotBlank(message = "Please enter the Maven artifact ID")
-    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*(-[a-zA-Z0-9]+)*$", message = "Please enter a valid artifact ID")
+    @Pattern(regexp = ARTIFACT_ID, message = "Please enter a valid artifact ID")
     private String artifactId = "my-app";
+
     @NotBlank(message = "Please enter the Maven artifact's group ID")
-    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*(\\.[a-zA-Z0-9]+)*$", message = "Please enter a valid group ID")
+    @Pattern(regexp = PACKAGE_OR_GROUP_ID, message = "Please enter a valid group ID")
     private String groupId = "ch.admin.bit";
 
     private Map<String, String> templateParameterValues = new HashMap<>();
